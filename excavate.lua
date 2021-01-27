bf4fbcc51f08dc0e8bfc82be1e6fd736690a599c
Yuki <yuki@manjaro.Speedport_W_921V_1_46_000>
2021-01-27 17:00:55 +0100

    Fixed Down excavating for sure

--- excavate.lua
+++ excavate.lua
@@ -35,9 +35,13 @@ end
 
 stepTable = {}
 
-function table.clone(org) -- cheap way to copy tables
-    return {table.unpack(org)}
+function table.clone(obj)
+    if type(obj) ~= 'table' then return obj end
+    local res = {}
+    for k, v in pairs(obj) do res[table.clone(k)] = table.clone(v) end
+    return res
 end
+
 
 
 local tunnel = table.clone(move)
@@ -92,7 +96,6 @@ end
 
 function firstY(up,directionUp)
     print("what dir? "..up)
-    print(up*1)
     print(vector.new(excavate.offset.forward,excavate.offset.up,excavate.offset.left))
     if up*directionUp == 2 then
         --Goto Offset Position --
@@ -106,17 +109,17 @@ function firstY(up,directionUp)
         end
 
         lastMove = #stepTable
-        printWholeList(stepTable)
+        --printWholeList(stepTable)
         if lastMove > 0 then
             stepTable[lastMove].digN = digNum
         end
-        printWholeList(stepTable)
+        --printWholeList(stepTable)
         calc.forwardAxis(baseAxis,1,true,digNum) --dig a layer forward and (down or up)
         cleared=2
     elseif up*directionUp > 2 then -- 3 or above
         --Goto Offset Position --
-        print("xxs: ",up)
-        print(virtLocation)
+        --print("xxs: ",up)
+        --print(virtLocation)
         virtLocation = virtLocation + vector.new(excavate.offset.forward,excavate.offset.up,excavate.offset.left) --go into the area
         insertPos(stepTable,virtLocation,1)
         -------------------------
@@ -143,31 +146,40 @@ function firstY(up,directionUp)
 end
 
 function loopY(up,forwardDir,directionUp)
-
-    if up == 2*directionUp then
-
+    print("xxs: ",up)
+    if up*directionUp == 2 then
+        print("dig 2")
+        if directionUp < 0 then
+            digNum = 2
+        else
+            digNum = 3
+        end
+        stepTable[#stepTable].digN = digNum
+        --[[
         if directionUp > 0 then -- when it's +1
             digNum = 2 -- dig forward and up
         else                    -- when it's -1
+            print("zz_3")
             digNum = 3 -- dig forward and downs
-        end
-        calc.forwardAxis(baseAxis,forwardDir,true,digNum) --dig forward and up or down
+        end]]
+        calc.forwardAxis(baseAxis,forwardDir,true,2) --dig forward and up or down
         --virtLocation = virtLocation + vector.new(0,1,0)
         --insertPos(stepTable,virtLocation,4)
         cleared=2
-    elseif up > 2*directionUp then
-
+    elseif up*directionUp > 2 then
+        print("dig 3")
         calc.forwardAxis(baseAxis,forwardDir,true,4)
         if (up-3)*directionUp > 2 then
-            virtLocation = virtLocation + vector.new(0,3*directionUp,0) --go three up
+            virtLocation = virtLocation + vector.new(0,3*directionUp,0) --go three up (or down)
             insertPos(stepTable,virtLocation,4)
         elseif (up-3)*directionUp > 0 then
-            virtLocation = virtLocation + vector.new(0,2*directionUp,0) --go three up
+            virtLocation = virtLocation + vector.new(0,2*directionUp,0) --go three up (or down)
             insertPos(stepTable,virtLocation,4)
         end
         cleared=3
         --printWholeList(stepTable)
     else --probably up = 1
+        print("dig 1")
         calc.forwardAxis(baseAxis,forwardDir,true,1) --dig only forward
         --virtLocation = virtLocation + vector.new(0,1*directionUp,0)
         --insertPos(stepTable,virtLocation,1)
@@ -178,7 +190,7 @@ end
 
 function wholeLoop(remaining,forwardDir,directionUp)
     --print("Remaining:"..remaining)
-    cleared = loopY(remaining,forwardDir,directionUp)
+    cleared = loopY(remaining*directionUp,forwardDir,directionUp)
     remaining = remaining - cleared
     print(remaining,cleared)
     if forwardDir == 1 then
@@ -189,26 +201,27 @@ function wholeLoop(remaining,forwardDir,directionUp)
     return remaining,forwardDir
 end
 
--- Precalculate the Room that will me digged --
+-- Precalculate the Room that will be digged --
 ---------------------------------------------------------
 -- It will store every coordinate in table "stepTable" --
-
+---------------------------------------------------------
 function calcRoom()
     virtLocation = turtle.location -- save the current location as a virtual location
     baseAxis= turtle.location.z -- get the axis that will be used to go Left later. For now it's static
-    forwardDir=-1
+    forwardDir=-1 -- forward Direction. It will be used after the FirstY so the turtle goes to the negative direction back
     -- check if up is inverted
     if excavate.up > 0 then -- it's 1 or above
         cleared = firstY(excavate.up,1) -- Excavate the first Layer and return the layers cleared.
         remaining = excavate.up - cleared
         while remaining > 0 do 
-            print("Remaining: "..remaining)
+            --print("Remaining: "..remaining)
             remaining,forwardDir = wholeLoop(remaining,forwardDir,1)
         end
     elseif excavate.up < 0 then -- it's -1 or under
         cleared = firstY(excavate.up,-1) -- Excavate the first Layer and return the layers cleared.
-        remaining = excavate.up - cleared
-        print("Remaining: "..remaining)
+        remaining = excavate.up*-1 - cleared
+        print("Remaining: "..remaining.." and "..excavate.up,cleared)
+        print("forwardDir = "..forwardDir)
         while remaining > 0 do
             remaining,forwardDir = wholeLoop(remaining,forwardDir,-1)
         end
@@ -239,10 +252,12 @@ function execute46(stepTable)
     for i,v in pairs(stepTable) do
         dest = v.position - turtle.location -- get delta to the position you are going to
         --print(dest,v.position,turtle.location)
+        print("table:Dig:"..v.digN)
+        printWholeList(digFunc[v.digN])
         Goto.facingFirst(dest,digFunc[v.digN],turtle.facing)
     end
 end
 
 --print(turtle.location)
 
---execute46(stepTable)
+execute46(stepTable)
