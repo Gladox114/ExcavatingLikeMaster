6d2daf858b4dc07725afd20d9a8a33e2d9f69ddd
Gladox114 <53704522+Gladox114@users.noreply.github.com>
2023-10-25 21:49:35 +0200

    Update installer

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 -- This file Downloads all files from github https://github.com/Gladox114/ExcavatingLikeMaster
 
 -- create a list full of files (excavate.lua, invCheck.lua, ...)
-
+local folder = "ExcavatingLikeMaster"
 local files = {
   "excavate.lua",
   "invCheck.lua",
@@ -17,8 +17,9 @@ local files = {
 
 -- use wget and github raw page to download each file
 
+shell.run("mkdir " .. folder)
 
 for i = 1, #files do
   shell.run("wget https://raw.githubusercontent.com/Gladox114/ExcavatingLikeMaster/master/" ..
-    files[i] .. " " .. files[i])
+    files[i] .. " " .. folder .. " " .. files[i])
 end
